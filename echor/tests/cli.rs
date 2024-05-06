f544d8b2f00ef0fe785c206e2e3b6dd93f9ddfb0
radiol <radiol@github.com>
2024-05-07 02:00:34 +0900

    第2章: echo: テスト記述

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -2,61 +2,43 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 use std::fs;
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.assert()
+fn dies_no_args() -> TestResult {
+    Command::cargo_bin("echor")?
+        .assert()
         .failure()
         .stderr(predicate::str::contains("Usage:"));
+    Ok(())
 }
 
-#[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("hello").assert().success();
-}
-
-#[test]
-fn hello1() {
-    let outfile = "tests/expects/hello1.txt";
-    let expected = fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("Hello there").assert().success().stdout(expected);
-}
-
-#[test]
-fn hello2() {
-    let outfile = "tests/expects/hello2.txt";
-    let expected = fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("Hello")
-        .arg("there")
+fn runs(args: &[&str], expected_file: &str) -> TestResult {
+    let expect = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echor")?
+        .args(args)
         .assert()
         .success()
-        .stdout(expected);
+        .stdout(expect);
+    Ok(())
 }
 
 #[test]
-fn hello1n() {
-    let outfile = "tests/expects/hello1.n.txt";
-    let expected = fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("-n")
-        .arg("Hello there")
-        .assert()
-        .success()
-        .stdout(expected);
+fn hello1() -> TestResult {
+    runs(&["Hello there"], "tests/expects/hello1.txt")
 }
 
 #[test]
-fn hello2n() {
-    let outfile = "tests/expects/hello2.n.txt";
-    let expected = fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("-n")
-        .arg("Hello")
-        .arg("there")
-        .assert()
-        .success()
-        .stdout(expected);
+fn hello2() -> TestResult {
+    runs(&["Hello", "there"], "tests/expects/hello2.txt")
+}
+
+#[test]
+fn hello1n() -> TestResult {
+    runs(&["-n", "Hello there"], "tests/expects/hello1.n.txt")
+}
+
+#[test]
+fn hello2n() -> TestResult {
+    runs(&["-n", "Hello", "there"], "tests/expects/hello2.n.txt")
 }
